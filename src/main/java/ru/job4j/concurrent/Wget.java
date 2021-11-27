cfe6d37dc165e59a19c2f54ef3fb4761efc82250
shabelnikilya <shabelnik_ilya@mail.ru>
2021-11-27 19:14:41 +0300

    1.2. Режим ожидания. [#231217]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -2,7 +2,7 @@ package ru.job4j.concurrent;
 
 public class Wget {
     public static void main(String[] args) {
-        Thread thread = new Thread (
+        Thread thread = new Thread(
                 () -> {
                     try {
                         for (int i = 0; i < 101; i++) {
