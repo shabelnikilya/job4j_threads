946486746f1aa7d4ec53965b3971c63ad3575530
shabelnikilya <shabelnik_ilya@mail.ru>
2021-11-28 14:03:03 +0300

    1.1. Состояние нити. [#229175 #233405] - изменения

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -11,7 +11,7 @@ public class ThreadState {
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED
-                && second.getState() != Thread.State.TERMINATED) {
+                || second.getState() != Thread.State.TERMINATED) {
             System.out.println(first.getState() + System.lineSeparator() + second.getState());
         }
         System.out.println("Работа завершена");
