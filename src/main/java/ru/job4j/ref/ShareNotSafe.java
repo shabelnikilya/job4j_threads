f6dbcceaa40735ae65fe79b03dee31ffae99cdf9
shabelnikilya <shabelnik_ilya@mail.ru>
2022-01-10 19:32:24 +0300

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ru/job4j/ref/ShareNotSafe.java
@@ -0,0 +1,15 @@
+package ru.job4j.ref;
+
+public class ShareNotSafe {
+    public static void main(String[] args) throws InterruptedException {
+        UserCache cache = new UserCache();
+        User user = User.of("name");
+        cache.add(user);
+        Thread first = new Thread(
+                () -> cache.findAll().get(0).setName("Ilya")
+        );
+        first.start();
+        first.join();
+        System.out.println(cache.findAll());
+    }
+}
