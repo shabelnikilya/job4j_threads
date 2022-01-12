cb2854a480545ee6cde4b5f482964a2453197c72
shabelnikilya <shabelnik_ilya@mail.ru>
2022-01-12 10:11:22 +0300

    2. JCIP. Настройка библиотеки [#268575]

---
+++ src/test/java/ru/job4j/concurrent/CountTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.concurrent;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CountTest {
+
+    private class ThreadCount extends Thread {
+        private final Count count;
+
+        public ThreadCount(Count count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            this.count.increment();
+        }
+    }
+
+    @Test
+    public void whenExecute2ThreadThen2() throws InterruptedException {
+        final Count count = new Count();
+        Thread first = new ThreadCount(count);
+        Thread second = new ThreadCount(count);
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        assertThat(count.get(), is(2));
+    }
+}
