bebf087e017350debf4966449c2266dcdd43b634
shabelnikilya <shabelnik_ilya@mail.ru>
2022-01-19 11:21:33 +0300

    2. ExecutorService рассылка почты. [#63097 #252840]

--- src/main/java/ru/job4j/pool/EmailNotification.java
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -4,6 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class EmailNotification {
+
     private final ExecutorService pool;
 
     public EmailNotification(ExecutorService pool) {
---
+++ src/main/java/ru/job4j/pool/User.java
@@ -0,0 +1,27 @@
+package ru.job4j.pool;
+
+public class User {
+    private final String username;
+    private final String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
