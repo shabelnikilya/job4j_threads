c3ee1fcbcf73d7c751b3719d76b8810e95b08f53
shabelnikilya <shabelnik_ilya@mail.ru>
2022-01-19 11:02:58 +0300

    1. Реализовать ThreadPool [#1099]

---
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -0,0 +1,50 @@
+package ru.job4j.pool;
+
+import net.jcip.annotations.ThreadSafe;
+import ru.job4j.synch.SimpleBlockingQueue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ThreadSafe
+public class ThreadPool {
+    private final List<Thread> threads;
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>();
+
+    public ThreadPool(int size) {
+        this.threads = createNewThreadsAddToListAndStart(size);
+    }
+
+    public List<Thread> createNewThreadsAddToListAndStart(int size) {
+        List<Thread> pool = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            Thread thread = new Thread(() -> {
+                try {
+                    while (!tasks.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        tasks.poll().run();
+                    }
+                } catch (InterruptedException ie) {
+                    ie.printStackTrace();
+                    Thread.currentThread().interrupt();
+                }
+            }
+            );
+            pool.add(thread);
+            thread.start();
+        }
+        return pool;
+    }
+
+    public void work(Runnable job) {
+        try {
+            tasks.offer(job);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public void shutdown() {
+        threads.forEach(Thread::interrupt);
+    }
+}
---
+++ src/test/java/ru/job4j/pool/ThreadPoolTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.pool;
+
+import org.junit.Test;
+import ru.job4j.concurrent.Count;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ThreadPoolTest {
+    private final int size = Runtime.getRuntime().availableProcessors();
+
+    @Test
+    public void workAddThreeIncrement() {
+        Count count = new Count();
+        ThreadPool pool = new ThreadPool(size);
+        pool.work(count::increment);
+        pool.work(count::increment);
+        pool.work(count::increment);
+        pool.shutdown();
+        assertThat(count.get(), is(3));
+    }
+
+    @Test
+    public void workAddValueToList() {
+        List<String> str = new ArrayList<>();
+        ThreadPool pool = new ThreadPool(size);
+        pool.work(() -> str.add("test"));
+        pool.shutdown();
+        assertThat(str.get(0), is("test"));
+    }
+}
