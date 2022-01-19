ced377b20d8610b8ca9c40b73c0be817da18d392
shabelnikilya <shabelnik_ilya@mail.ru>
2022-01-19 11:51:28 +0300

    3. ForkJoinPool [#315067]

---
+++ src/main/java/ru/job4j/pool/sortForkJoin/ParallelSearchObj.java
@@ -0,0 +1,44 @@
+package ru.job4j.pool.sortForkJoin;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class ParallelSearchObj<V> extends RecursiveTask<Integer> {
+    private final V[] array;
+    private final V findValue;
+    private final int from;
+    private final int to;
+
+    public ParallelSearchObj(V[] array, V findValue, int from, int to) {
+        this.array = array;
+        this.findValue = findValue;
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    protected Integer compute() {
+        if (to - from <= 10) {
+            return getIndexWhenSmallArray();
+        }
+        int mid = (from + to) / 2;
+        ParallelSearchObj<V> leftSearch = new ParallelSearchObj<>(array, findValue, from, mid);
+        ParallelSearchObj<V> rightSearch = new ParallelSearchObj<>(array, findValue, mid + 1, to);
+        leftSearch.fork();
+        rightSearch.fork();
+        return Math.max(leftSearch.join(), rightSearch.join());
+    }
+
+    public int getIndexWhenSmallArray() {
+        for (int i = from; i <= to; i++) {
+            if (findValue.equals(array[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int  getIndexObject() {
+        return new ForkJoinPool().invoke(new ParallelSearchObj<>(array, findValue, from, to));
+    }
+}
---
+++ src/test/java/ru/job4j/pool/sortForkJoin/ParallelSearchObjTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.pool.sortForkJoin;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ParallelSearchObjTest {
+    private final Integer[] array = {1, 3, 5, 7, 9, 11, 13, 2, 4, 6, 8, 10, 12};
+
+    @Test
+    public void whenFindIntegerObject() {
+        ParallelSearchObj<Integer> searchObj = new ParallelSearchObj<>(
+                array, 12, 0, array.length - 1);
+        assertThat(searchObj.getIndexObject(), is(12));
+    }
+
+    @Test
+    public void whenNotFindIntegerObjectMinusOne() {
+        ParallelSearchObj<Integer> searchObj = new ParallelSearchObj<>(
+                array, 66, 0, array.length - 1);
+        assertThat(searchObj.getIndexObject(), is(-1));
+    }
+
+    @Test
+    public void whenFindIntegerObjectInSmallPartArray() {
+        ParallelSearchObj<Integer> searchObj = new ParallelSearchObj<>(
+                array, 3, 0, 5);
+        assertThat(searchObj.getIndexObject(), is(1));
+    }
+
+    @Test
+    public void whenFromEqualsToMinusOne() {
+        ParallelSearchObj<Integer> searchObj = new ParallelSearchObj<>(
+                array, 3, 0, 0);
+        assertThat(searchObj.getIndexObject(), is(-1));
+    }
+
+    @Test
+    public void whenFromEqualsToAndFindObject() {
+        ParallelSearchObj<Integer> searchObj = new ParallelSearchObj<>(
+                array, 1, 0, 0);
+        assertThat(searchObj.getIndexObject(), is(0));
+    }
+}
