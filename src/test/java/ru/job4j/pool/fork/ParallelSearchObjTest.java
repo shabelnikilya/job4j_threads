b0566b18b29d2937270110d4bc6f54c686160777
shabelnikilya <shabelnik_ilya@mail.ru>
2022-01-20 08:06:19 +0300

    3. ForkJoinPool [#315067 #252849] - изменения.

--- src/test/java/ru/job4j/pool/fork/ParallelSearchObjTest.java
+++ src/test/java/ru/job4j/pool/fork/ParallelSearchObjTest.java
@@ -6,55 +6,30 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class ParallelSearchObjTest {
-    private final Integer[] array = {1, 3, 5, 7, 9, 11, 13, 2, 4, 6, 8, 10, 12};
 
     @Test
     public void whenFindIntegerObject() {
+        final Integer[] array = {1, 3, 5, 7, 9, 11, 13, 2, 4, 6, 8, 10, 12};
         assertThat(
-                ParallelSearchObj.getIndexObject(
-                    array,
-                    12,
-                    0,
-                    array.length - 1), is(12));
+                ParallelSearchObj.getIndexObject(array,12), is(12));
     }
 
     @Test
     public void whenNotFindIntegerObjectMinusOne() {
+        final Integer[] array = {1, 3, 5, 7, 9, 11, 13, 2, 4, 6, 8, 10, 12};
         assertThat(
-                ParallelSearchObj.getIndexObject(
-                        array,
-                        66,
-                        0,
-                        array.length - 1), is(-1));
-    }
-
-    @Test
-    public void whenFindIntegerObjectInSmallPartArray() {
-        assertThat(
-                ParallelSearchObj.getIndexObject(
-                        array,
-                        3,
-                        0,
-                        5), is(1));
+                ParallelSearchObj.getIndexObject(array,66), is(-1));
     }
 
     @Test
     public void whenFromEqualsToMinusOne() {
-        assertThat(
-                ParallelSearchObj.getIndexObject(
-                        array,
-                        3,
-                        0,
-                        0), is(-1));
+        Integer[] oneElementArray = {1};
+        assertThat(ParallelSearchObj.getIndexObject(oneElementArray,3), is(-1));
     }
 
     @Test
     public void whenFromEqualsToAndFindObject() {
-        assertThat(
-                ParallelSearchObj.getIndexObject(
-                        array,
-                        1,
-                        0,
-                        0), is(0));
+        Integer[] oneElementArray = {1};
+        assertThat(ParallelSearchObj.getIndexObject(oneElementArray,1), is(0));
     }
 }
