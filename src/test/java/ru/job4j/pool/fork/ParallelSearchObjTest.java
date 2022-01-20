6cd29890d4d582e2b93881201948f0f59291a43d
shabelnikilya <shabelnik_ilya@mail.ru>
2022-01-20 08:15:42 +0300

    3. ForkJoinPool [#315067 #252849]

--- src/test/java/ru/job4j/pool/fork/ParallelSearchObjTest.java
+++ src/test/java/ru/job4j/pool/fork/ParallelSearchObjTest.java
@@ -10,25 +10,25 @@ public class ParallelSearchObjTest {
     @Test
     public void whenFindIntegerObject() {
         final Integer[] array = {1, 3, 5, 7, 9, 11, 13, 2, 4, 6, 8, 10, 12};
-        assertThat(ParallelSearchObj.getIndexObject(array,12), is(12));
+        assertThat(ParallelSearchObj.getIndexObject(array, 12), is(12));
     }
 
     @Test
     public void whenNotFindIntegerObjectMinusOne() {
         final Integer[] array = {1, 3, 5, 7, 9, 11, 13, 2, 4, 6, 8, 10, 12};
         assertThat(
-                ParallelSearchObj.getIndexObject(array,66), is(-1));
+                ParallelSearchObj.getIndexObject(array, 66), is(-1));
     }
 
     @Test
     public void whenFromEqualsToMinusOne() {
         Integer[] oneElementArray = {1};
-        assertThat(ParallelSearchObj.getIndexObject(oneElementArray,3), is(-1));
+        assertThat(ParallelSearchObj.getIndexObject(oneElementArray, 3), is(-1));
     }
 
     @Test
     public void whenFromEqualsToAndFindObject() {
         Integer[] oneElementArray = {1};
-        assertThat(ParallelSearchObj.getIndexObject(oneElementArray,1), is(0));
+        assertThat(ParallelSearchObj.getIndexObject(oneElementArray, 1), is(0));
     }
 }
